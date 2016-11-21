ab48a5adb8a39e4aef02adcb9f2ee37766dfc874
Werner Schram <wrschram@gmail.com>
2016-11-21 22:23:58 +0100

    applied Static to Push.

--- assembler.x86/src/main/scala/assembler/x86/instructions/stack/Push.scala
+++ assembler.x86/src/main/scala/assembler/x86/instructions/stack/Push.scala
@@ -1,13 +1,14 @@
 package assembler.x86.instructions.stack
 
 import assembler.x86.ProcessorMode
-import assembler.x86.opcodes.Static
 import assembler.x86.operands.ModRMEncodableOperand
 import assembler.x86.operands.FixedSizeModRMEncodableOperand
 import assembler.x86.operands.ImmediateValue
 import assembler.x86.operands._
 import assembler.x86.operations.ModRMStaticOperation
 import assembler.x86.operations.RegisterEncoded
+import assembler.x86.operations.Static
+import assembler.x86.operations.Immediate
 
 final object Push {
   implicit val opcode = "push"
@@ -19,31 +20,39 @@ final object Push {
     case _ => true
   }
 
-  private def R16(register: WideRegister)(implicit processorMode: ProcessorMode)  =
+  private def R16(register: WideRegister)(implicit processorMode: ProcessorMode) =
     new RegisterEncoded[WideRegister](register, 0x50.toByte :: Nil, opcode, includeRexW = false) {
-    override def validate = {
-      super.validate
-      assume(lengthModeValidation(processorMode, register))
+      override def validate = {
+        super.validate
+        assume(lengthModeValidation(processorMode, register))
+      }
     }
-  }
 
   private def RM16(operand: FixedSizeModRMEncodableOperand)(implicit processorMode: ProcessorMode) =
     new ModRMStaticOperation(operand, 0xFF.toByte :: Nil, 0x06.toByte, opcode) {
       assume(lengthModeValidation(processorMode, operandRM))
     }
 
-  private val Imm8 = new Static(0x6A.toByte :: Nil).withImmediate()
-  private val Imm16 = new Static(0x68.toByte :: Nil).withImmediate({ case (value, mode) => value.operandByteSize != 8; case _ => true })
+  private def Imm8(immediateValue: ImmediateValue)(implicit processorMode: ProcessorMode) = new Static(0x6A.toByte :: Nil, opcode) with Immediate {
+    override def immediate = immediateValue
+  }
+  private def Imm16(immediateValue: ImmediateValue)(implicit processorMode: ProcessorMode) = new Static(0x68.toByte :: Nil, opcode) with Immediate {
+    override def immediate = immediateValue
+    override def validate = {
+      super.validate
+      assume(immediate.operandByteSize != 8)
+    }
+  }
 
-  private val StaticCS = new Static(0x0E.toByte :: Nil)
-  private val StaticSS = new Static(0x16.toByte :: Nil)
-  private val StaticDS = new Static(0x1E.toByte :: Nil)
-  private val StaticES = new Static(0x06.toByte :: Nil)
-  private val StaticFS = new Static(0x0F.toByte :: 0xA0.toByte :: Nil)
-  private val StaticGS = new Static(0x0F.toByte :: 0xA8.toByte :: Nil)
+  private def StaticCS()(implicit processorMode: ProcessorMode) = new Static(0x0E.toByte :: Nil, opcode)
+  private def StaticSS()(implicit processorMode: ProcessorMode) = new Static(0x16.toByte :: Nil, opcode)
+  private def StaticDS()(implicit processorMode: ProcessorMode) = new Static(0x1E.toByte :: Nil, opcode)
+  private def StaticES()(implicit processorMode: ProcessorMode) = new Static(0x06.toByte :: Nil, opcode)
+  private def StaticFS()(implicit processorMode: ProcessorMode) = new Static(0x0F.toByte :: 0xA0.toByte :: Nil, opcode)
+  private def StaticGS()(implicit processorMode: ProcessorMode) = new Static(0x0F.toByte :: 0xA8.toByte :: Nil, opcode)
 
-  //  def apply(register: WideRegister)(implicit processorMode: ProcessorMode) =
-  //    R16(register)
+  def apply(register: WideRegister)(implicit processorMode: ProcessorMode) =
+    R16(register)
 
   def apply(operand: FixedSizeModRMEncodableOperand)(implicit processorMode: ProcessorMode) = operand match {
     case register: WideRegister =>
@@ -52,9 +61,10 @@ final object Push {
       RM16(operand)
   }
 
-  def apply(immediate: ImmediateValue)(implicit processorMode: ProcessorMode) = immediate match {
-    case immediate: ImmediateValue if (immediate.operandByteSize == 1) => Imm8(immediate)
-    case immediate: ImmediateValue if (immediate.operandByteSize > 1) => Imm16(immediate)
+  def apply(immediate: ImmediateValue)(implicit processorMode: ProcessorMode) = immediate.operandByteSize match {
+    case 1 => Imm8(immediate)
+    case 2 | 4 => Imm16(immediate)
+    case 8 => throw new AssertionError
   }
 
   def apply(segment: SegmentRegister)(implicit processorMode: ProcessorMode) = segment match {
