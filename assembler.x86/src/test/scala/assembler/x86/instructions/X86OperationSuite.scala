99712e90b5d2d677d6867aa6b8218c32c6579352
Werner Schram <wrschram@gmail.com>
2016-12-04 10:48:24 +0100

    Added coverage for X86Operation.

---
+++ assembler.x86/src/test/scala/assembler/x86/instructions/X86OperationSuite.scala
@@ -0,0 +1,70 @@
+package assembler.x86.operations
+
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.ShouldMatchers
+import org.scalatest.WordSpec
+import assembler.memory.MemoryPage
+import assembler.x86.ProcessorMode
+import assembler.x86.instructions.Interrupt
+
+class X86OperationSuite extends WordSpec with ShouldMatchers with MockFactory {
+
+  implicit val page: MemoryPage = new MemoryPage(List.empty[X86Operation])
+
+  "an X86 instruction" when {
+    "in protected mode" should {
+
+      class MyInstruction extends X86Operation {
+        override def code = 0x00.toByte :: Nil
+        override def mnemonic = "mis"
+        override def operands = Nil
+        override implicit val processorMode: ProcessorMode = ProcessorMode.Protected
+
+      }
+
+      "return the size of the instruction" in {
+        val instruction = new MyInstruction()
+        instruction.size() should be(1)
+      }
+
+      "return a labeled instruction for the given instruction" in {
+        val instruction = new MyInstruction()
+
+        val labeledInstruction = instruction.withLabel("Label")
+
+        labeledInstruction.label.toString() should be("Label")
+      }
+
+    }
+  }
+
+  "a Labeled X86 operation" when {
+    "in protected mode" should {
+
+      "correctly return the size of the instruction" in {
+        val instruction = stub[X86Operation]
+        (instruction.size()(_: MemoryPage)).when(*).returns(4)
+
+        val labeledInstruction = new LabeledX86Operation(instruction, "Label")
+        labeledInstruction.size() should be(4)
+      }
+
+      "return the encoded instruction when encodeByte is called" in {
+        val instruction = stub[X86Operation]
+        (instruction.encodeByte()(_: MemoryPage)).when(*).returns(0x01.toByte :: 0x02.toByte :: Nil)
+
+        val labeledInstruction = new LabeledX86Operation(instruction, "Label")
+        labeledInstruction.encodeByte() should be(0x01.toByte :: 0x02.toByte :: Nil)
+      }
+
+      "correctly represent the instruction as a string" in {
+        implicit val processorMode = ProcessorMode.Protected
+
+        val instruction = Interrupt(3)
+
+        val labeledInstruction = new LabeledX86Operation(instruction, "Label")
+        labeledInstruction.toString() should be("Label: int 3")
+      }
+}
+  }
+}
