12841b70a3a7fb3a5271e18a8ac9af64aad65590
Werner Schram <wrschram@gmail.com>
2016-12-09 13:05:11 +0100

    changed relativePointer to work with unshifted parameter

--- assembler.arm/src/main/scala/assembler/arm/operands/RelativePointer.scala
+++ assembler.arm/src/main/scala/assembler/arm/operands/RelativePointer.scala
@@ -3,13 +3,14 @@ package assembler.arm.operands
 import scala.language.implicitConversions
 
 class RelativePointer private(val displacement: Int) extends Operand {
-  assume((displacement > -8388608) && (displacement < 8388607))
+  assume(((displacement & 0xC0000000) == 0) || ((displacement & 0xC0000000) == 0xC0000000))
+  assume((displacement & 0x00000003) == 0)
 
-  def encode = displacement & 0xFFFFFF
+  def encode = (displacement >> 2) & 0xFFFFFF
 
-  override def toString() = s"${(displacement * 4).toString()}"
+  override def toString() = s"${displacement.toString()}"
 }
 
 object RelativePointer {
-  implicit def apply(displacement: Int) = new RelativePointer(displacement/4)
+  implicit def apply(displacement: Int) = new RelativePointer(displacement)
 }
