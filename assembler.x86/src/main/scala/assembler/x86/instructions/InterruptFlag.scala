05ac5d3fb8d4d873a01951d545a4cf15f32784ed
Werner Schram <wrschram@gmail.com>
2016-12-28 09:23:27 +0100

    moved clear/setInterruptFlag to a single file.

--- assembler.x86/src/main/scala/assembler/x86/instructions/InterruptFlag.scala
+++ assembler.x86/src/main/scala/assembler/x86/instructions/InterruptFlag.scala
@@ -3,6 +3,15 @@ package assembler.x86.instructions
 import assembler.x86.ProcessorMode
 import assembler.x86.operations.Static
 
+object ClearInterruptFlag {
+  implicit val opcode = "cli"
+
+  private def Static()(implicit processorMode: ProcessorMode) = new Static(0xFA.toByte :: Nil, opcode)
+
+  def apply()(implicit processorMode: ProcessorMode) =
+    Static()
+}
+
 object SetInterruptFlag {
   implicit val mnemonic = "sti"
 
--- assembler.x86/src/test/scala/assembler/x86/instructions/InterruptFlagSuite.scala
+++ assembler.x86/src/test/scala/assembler/x86/instructions/InterruptFlagSuite.scala
@@ -1,20 +1,39 @@
-package assembler.x86.instructions.interrupt
+package assembler.x86.instructions
 
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.ShouldMatchers
 import org.scalatest.WordSpec
-
 import assembler.Hex
 import assembler.memory.MemoryPage
 import assembler.x86.ProcessorMode
 import assembler.x86.operations.X86Operation
-import assembler.x86.instructions.SetInterruptFlag
 
-class SetInterruptFlagSuite extends WordSpec with ShouldMatchers with MockFactory {
+class ClearInterruptFlagSuite extends WordSpec with ShouldMatchers with MockFactory {
 
   implicit val page: MemoryPage = new MemoryPage(List.empty[X86Operation])
 
-  "an SetInterruptFlag instruction" when {
+  "a ClearInterruptFlag instruction" when {
+
+    "in real mode" should {
+
+      implicit val processorMode = ProcessorMode.Real
+
+      "correctly encode cli" in {
+        ClearInterruptFlag().encodeByte should be (Hex.lsb("FA"))
+      }
+    }
+
+    "in long mode" should {
+
+      implicit val processorMode = ProcessorMode.Long
+
+      "correctly encode cli" in {
+        ClearInterruptFlag().encodeByte should be (Hex.lsb("FA"))
+      }
+    }
+  }
+
+  "a SetInterruptFlag instruction" when {
 
     "in real mode" should {
 
