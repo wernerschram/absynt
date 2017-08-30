26472a3e94dac2131cffcd7595babf514e5d13de
Werner Schram <werner.schram@simacan.com>
2017-08-30 08:08:16 +0200

    fixed some issues in the RPI example and readded the raw output as it seems to be usefull in qemu

--- examples/arm/bootRpi/src/main/scala/examples/assembler/arm/Boot.scala
+++ examples/arm/bootRpi/src/main/scala/examples/assembler/arm/Boot.scala
@@ -16,6 +16,7 @@ object Boot extends App {
 
   object GPIO {
     val Base = 0x20200000
+    //val Base = 0x101f1000
 
     val GPPUD: Short = 0x94.toShort
     val GPPUDCLK0: Short = 0x98.toShort
@@ -47,29 +48,18 @@ object Boot extends App {
 
   object elf {
     def magic: List[Byte] = 0x7F.toByte :: Nil ::: "ELF".toCharArray.map(_.toByte).toList
-
     def `class`: List[Byte] = 0x01.toByte :: Nil // 32bit
-
     def data: List[Byte] = 0x01.toByte :: Nil // big endian
-
     def version: List[Byte] = 0x01.toByte :: Nil
-
     def osAbi: List[Byte] = 0x00.toByte :: Nil
-
     def abiVersion: List[Byte] = 0x00.toByte :: Nil
-
     def eiPad: List[Byte] = List.fill(7)(0x00.toByte)
-
     def eType: List[Byte] = 0x02.toByte :: 0x00.toByte :: Nil // ???
-
     def eMachine: List[Byte] = 0x89.toByte :: 0x00.toByte :: Nil // ARM
-
     def eVersion: List[Byte] = 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
-
     def entry: List[Byte] = List.fill(1)(0x00.toByte) ::: 0x02.toByte :: 0xc0.toByte :: 0xce.toByte :: Nil
     def phoff: List[Byte] = 0x34.toByte :: Nil ::: List.fill(3)(0x00.toByte)
     def shoff: List[Byte] = 0x54.toByte :: Nil ::: List.fill(3)(0x00.toByte)
-
     def flags: List[Byte] = 0x00.toByte :: 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
     def ehSize: List[Byte] = 0x34.toByte :: 0x00.toByte :: Nil
     def phEntSize: List[Byte] = 0x20.toByte :: 0x00.toByte :: Nil
@@ -83,8 +73,8 @@ object Boot extends App {
       def offset: List[Byte] = 0xcc.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def vaddr: List[Byte] = 0x00.toByte :: 0x1e.toByte :: 0xc0.toByte :: 0xce.toByte :: Nil
       def paddr: List[Byte] = 0x00.toByte :: 0x1e.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
-      def filesz: List[Byte] = 0xc8.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
-      def memsz: List[Byte] = 0xc8.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
+      def filesz: List[Byte] = 0xb0.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
+      def memsz: List[Byte] = 0xb0.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def flags: List[Byte] = 0x05.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def allign: List[Byte] = 0x40.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
 
@@ -99,13 +89,17 @@ object Boot extends App {
         allign
     }
 
+    object nullSection {
+      def header: List[Byte] = List.fill(40)(0x00.toByte)
+    }
+
     object textSectionHeader {
       def name: List[Byte] = 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def `type`: List[Byte] = 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def flags: List[Byte] = 0x06.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def addr: List[Byte] = 0x00.toByte :: 0x1e.toByte :: 0xc0.toByte :: 0xce.toByte :: Nil
       def offset: List[Byte] = 0xcc.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
-      def filesz: List[Byte] = 0xc8.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
+      def filesz: List[Byte] = 0xb0.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def link: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def info: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def allign: List[Byte] = 0x40.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
@@ -124,16 +118,12 @@ object Boot extends App {
         entSize
     }
 
-    object nullSection {
-      def header: List[Byte] = List.fill(40)(0x00.toByte)
-    }
-
     object stringSectionHeader {
       def name: List[Byte] = 0x07.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def `type`: List[Byte] = 0x03.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def flags: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def addr: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
-      def offset: List[Byte] = 0x94.toByte :: 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
+      def offset: List[Byte] = 0x7c.toByte :: 0x01.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def filesz: List[Byte] = 0x11.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def link: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
       def info: List[Byte] = 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: 0x00.toByte :: Nil
@@ -198,33 +188,32 @@ object Boot extends App {
 
     val putString: Label = "PutString"
     val text: Label = "Text"
-    val targetLabel = Label.unique
 
     val page: Section = Section(
-      { implicit val label =  targetLabel; EncodedByteList(List.fill(4)(0x00.toByte))} ::
-      Add.forRelativeLabel(R0, targetLabel, R1) ::
-      Move.forConstant(4, R1) ::
       // Disable UART0
       Move.forConstant(UART0.Base, R0) ::
       Move.forConstant(0, R1) ::
       StoreRegister(R1, R0, UART0.CR) ::
+      naiveDelay(150, R4) :::
       //
       // Disable pull up/down for all GPIO pins & delay for 150 cycles.
       Move.forConstant(GPIO.Base, R2) ::
       //
       StoreRegister(R1, R2, GPIO.GPPUD) ::
-      naiveDelay(150, R1) :::
+      naiveDelay(150, R4) :::
       //
       // Disable pull up/down for pin 14,15 & delay for 150 cycles.
       Move.forConstant(3 << 14, R3) ::
       StoreRegister(R3, R2, GPIO.GPPUDCLK0) ::
-      naiveDelay(150, R1) :::
+      naiveDelay(150, R4) :::
       //
       // Write 0 to GPPUDCLK0 to make it take effect.
       StoreRegister(R1, R2, GPIO.GPPUDCLK0) ::
+      naiveDelay(150, R4) :::
       // Clear pending interrupts.
       Move.forConstant(0x7FF, R1) ::
       StoreRegister(R1, R0, UART0.ICR) ::
+      naiveDelay(150, R4) :::
       //
       // Set integer & fractional part of baud rate.
       // Divider = UART_CLOCK/(16 * Baud)
@@ -235,20 +224,25 @@ object Boot extends App {
       // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
       Move.forConstant(1, R1) ::
       StoreRegister(R1, R0, UART0.IBRD) ::
+        naiveDelay(150, R4) :::
       //
       // Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
       Move.forConstant(40, R1) ::
       StoreRegister(R1, R0, UART0.FBRD) ::
+        naiveDelay(150, R4) :::
       //
       // Mask all interrupts.
       Move.forConstant(0x70, R1) ::
       StoreRegister(R1, R0, UART0.LCRH) ::
+        naiveDelay(150, R4) :::
       Move.forConstant(0x7F1, R1) ::
       StoreRegister(R1, R0, UART0.IMSC) ::
+        naiveDelay(150, R4) :::
       //
       // Enable UART0, receive & transfer part of UART.
       Move.forConstant(0x7F1, R1) ::
       StoreRegister(R1, R0, UART0.CR) ::
+        naiveDelay(150, R4) :::
       //
       // Put the string from label [text] on the serial line
       Add.forRelativeLabel(PC, text, R7) ::
@@ -256,6 +250,7 @@ object Boot extends App {
       { implicit val label = putString; LoadRegister(R4, R0, UART0.FR)} ::
       LoadRegister(R5, R7, R6) ::
       StoreRegister(R5, R0, UART0.CR) ::
+        naiveDelay(150, R4) :::
       Add(R6, 1.toByte, R6) ::
       Compare(R6, 12.toByte) ::
       Branch(putString, NotEqual) ::
@@ -271,8 +266,11 @@ object Boot extends App {
     val outputPath = path.resolve("bootRpi-output")
     Files.createDirectories(outputPath)
     val outputFilePath = outputPath.resolve("test.elf")
+    val rawFilePath = outputPath.resolve("test.raw")
     val out = new FileOutputStream(outputFilePath.toFile)
-
+    val raw = new FileOutputStream(rawFilePath.toFile)
+    raw.write(page.encodeByte()(page).toArray)
+    println(s"size: ${page.size}")
     page.content.foreach { x => Console.println(s"${x.encodeByte()(page).bigEndianHexString} $x") }
     out.write((
       elf.header :::
