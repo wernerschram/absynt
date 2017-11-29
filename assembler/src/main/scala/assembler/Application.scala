f12d53452fec21ce3adb0be292694b2dfa0125ac
Werner Schram <werner.schram@simacan.com>
2017-11-29 09:07:01 +0100

    modified DistanceFunction to simplify its application

--- assembler/src/main/scala/assembler/Application.scala
+++ assembler/src/main/scala/assembler/Application.scala
@@ -40,6 +40,7 @@ abstract class Application[OffsetType<:Offset] protected (
     (dependent, independentDistance, offsetType)
   }
 
+  //TODO: calculate global shortest for references with UnknownDistance
   def encodablesForReferences(references: Seq[Reference]): Map[Reference, Encodable] = {
     val distanceFunctions: Map[Reference, DistanceFunction] =
       references.foldLeft(Map.empty[Reference, DistanceFunction])((x,y) => x ++ distanceFunctionsForDependencies(Set.empty, x)(y))
@@ -71,10 +72,55 @@ abstract class Application[OffsetType<:Offset] protected (
       ) yield
         t + (references.head._1 -> h)
 
-  private case class DistanceFunction(
-    requiredAssumptions: Set[Reference],
-    distance: Map[Reference, Int] => Int,
-    offsetDirection: OffsetDirection)
+  private sealed abstract class DistanceFunction(val offsetDirection: OffsetDirection) {
+    def requiredAssumptions: Set[Reference]
+    def distance(assumptions: Map[Reference, Int]): Int
+
+    def addDistanceFunction(reference: Reference, newDistanceFunction: DistanceFunction): DistanceFunction
+    def addDependency(reference: Reference): UnknownDistance
+  }
+
+  private case class KnownDistance(distance: Int, override val offsetDirection: OffsetDirection) extends DistanceFunction(offsetDirection) {
+    override def requiredAssumptions: Set[Reference] = Set.empty
+    override def distance(assumptions: Map[Reference, Int]): Int = distance
+
+    override def addDistanceFunction(reference: Reference, newDistanceFunction: DistanceFunction): DistanceFunction = {
+      val requiredAssumptions = this.requiredAssumptions ++ newDistanceFunction.requiredAssumptions
+      newDistanceFunction match {
+        case (newDistance: KnownDistance) =>
+          KnownDistance(distance + reference.sizeForDistance(newDistance.distance, offsetDirection), this.offsetDirection)
+        case (newDistance: UnknownDistance) =>
+          UnknownDistance(requiredAssumptions, (assumptions) =>
+            distance + reference.sizeForDistance(newDistance.distanceFunction(assumptions), newDistance.offsetDirection), offsetDirection)
+      }
+    }
+
+    override def addDependency(reference: Reference): UnknownDistance =
+      UnknownDistance(Set(reference), (assumptions) => assumptions(reference) + distance, offsetDirection)
+  }
+
+  private case class UnknownDistance(override val requiredAssumptions: Set[Reference],
+    distanceFunction: Map[Reference, Int] => Int, override val offsetDirection: OffsetDirection) extends DistanceFunction(offsetDirection) {
+
+    override def distance(assumptions: Map[Reference, Int]): Int = distanceFunction(assumptions)
+
+    override def addDistanceFunction(reference: Reference, newDistanceFunction: DistanceFunction): DistanceFunction = {
+      val requiredAssumptions = this.requiredAssumptions ++ newDistanceFunction.requiredAssumptions
+      newDistanceFunction match {
+        case (newDistance: KnownDistance) =>
+          val value = reference.sizeForDistance(newDistance.distance, newDistanceFunction.offsetDirection)
+          UnknownDistance(requiredAssumptions, (assumptions) =>
+            distanceFunction(assumptions) + value, offsetDirection)
+        case (newDistance: UnknownDistance) =>
+          UnknownDistance(requiredAssumptions, (assumptions) =>
+            distanceFunction(assumptions) + reference.sizeForDistance(newDistance.distanceFunction(assumptions), newDistance.offsetDirection), offsetDirection)
+      }
+    }
+
+    override def addDependency(reference: Reference): UnknownDistance =
+      UnknownDistance(requiredAssumptions + reference, (assumptions) =>
+        assumptions(reference) + distanceFunction(assumptions), offsetDirection)
+  }
 
   private final def distanceFunctionsForDependencies(visiting: Set[Reference],
     visited: Map[Reference, DistanceFunction])(current: Reference): Map[Reference, DistanceFunction] = {
@@ -86,10 +132,8 @@ abstract class Application[OffsetType<:Offset] protected (
       val (references, independentDistance, offsetDirection) = applicationContextProperties(current)
 
       val (distanceFunctions: Map[Reference, DistanceFunction], newDistanceFunction: DistanceFunction) =
-        references.foldLeft(
-          (visited,
-            DistanceFunction(Set.empty[Reference],
-            (_: Map[Reference, Int]) => independentDistance, offsetDirection))
+        references.foldLeft[(Map[Reference, DistanceFunction], DistanceFunction)](
+          (visited, KnownDistance(independentDistance, offsetDirection))
         ) {
           case (
             (previousDistanceFunctions: Map[Reference, DistanceFunction],
@@ -98,18 +142,12 @@ abstract class Application[OffsetType<:Offset] protected (
 
             if (visiting.contains(child))
               // cyclic dependency: add a dependency which can be resolved at a higher level
-              (previousDistanceFunctions,
-                DistanceFunction(previousDistance.requiredAssumptions + child,
-                  (assumptions) => previousDistance.distance(assumptions) + assumptions(child), offsetDirection))
+              (previousDistanceFunctions, previousDistance.addDependency(child))
             else {
-              val evaluatedDistanceFunctions: Map[Reference, DistanceFunction] =
+              val newDistanceFunctions = previousDistanceFunctions ++
                 distanceFunctionsForDependencies(visiting + current, previousDistanceFunctions)(child)
 
-              val newDistanceFunctions = previousDistanceFunctions ++ evaluatedDistanceFunctions
-              val childDistanceFunction: DistanceFunction = newDistanceFunctions(child)
-
-              (newDistanceFunctions,
-                incrementalDistanceForAssumption(child, previousDistance, childDistanceFunction))
+              (newDistanceFunctions, previousDistance.addDistanceFunction(child, newDistanceFunctions(child)))
             }
         }
       distanceFunctions + (current -> newDistanceFunction)
@@ -123,30 +161,6 @@ abstract class Application[OffsetType<:Offset] protected (
 //      }
 //      else
 //        distanceFunctions + (current -> distance)
-    }
-  }
-
-  private def incrementalDistanceForAssumption(
-    reference: Reference,
-    previousDistance: DistanceFunction,
-    newDistanceFunction: DistanceFunction): DistanceFunction = {
-    val requiredAssumptions = previousDistance.requiredAssumptions ++ newDistanceFunction.requiredAssumptions
-    (previousDistance.requiredAssumptions.toSeq, newDistanceFunction.requiredAssumptions.toSeq) match {
-      case (Nil, Nil) =>
-        val value = previousDistance.distance(Map.empty[Reference, Int]) +
-          reference.sizeForDistance(newDistanceFunction.distance(Map.empty[Reference, Int]), previousDistance.offsetDirection)
-        DistanceFunction(requiredAssumptions, (_: Map[Reference, Int]) => value, previousDistance.offsetDirection)
-      case (Nil, _) =>
-        val value = previousDistance.distance(Map.empty[Reference, Int])
-        DistanceFunction(requiredAssumptions, (assumptions: Map[Reference, Int]) =>
-          value + reference.sizeForDistance(newDistanceFunction.distance(assumptions), newDistanceFunction.offsetDirection), previousDistance.offsetDirection)
-      case (_, Nil) =>
-        val value = reference.sizeForDistance(newDistanceFunction.distance(Map.empty[Reference, Int]), newDistanceFunction.offsetDirection)
-        DistanceFunction(requiredAssumptions, (assumptions: Map[Reference, Int]) =>
-          previousDistance.distance(assumptions) + value, previousDistance.offsetDirection)
-      case (_, _) =>
-        DistanceFunction(requiredAssumptions, (assumptions: Map[Reference, Int]) =>
-          previousDistance.distance(assumptions) + reference.sizeForDistance(newDistanceFunction.distance(assumptions), newDistanceFunction.offsetDirection), previousDistance.offsetDirection)
     }
   }
 }
