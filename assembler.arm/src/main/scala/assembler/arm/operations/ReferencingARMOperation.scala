9a09d8c34cb88c80a7076bbd5112f939006af371
Werner Schram <werner.schram@simacan.com>
2017-11-30 07:34:15 +0100

    Fixed ARM Referencing instruction(s) size assumption

--- assembler.arm/src/main/scala/assembler/arm/operations/ReferencingARMOperation.scala
+++ assembler.arm/src/main/scala/assembler/arm/operations/ReferencingARMOperation.scala
@@ -2,7 +2,6 @@ package assembler.arm.operations
 
 import assembler._
 import assembler.arm.ArmOffsetFactory
-import assembler.arm.operands.ArmOffset
 import assembler.arm.operands.Condition.Condition
 import assembler.reference.SinglePassRelativeReference
 
@@ -11,12 +10,10 @@ abstract class ReferencingARMOperation(val label: Label, val opcode: String, ove
                                                    (implicit val offsetFactory: ArmOffsetFactory)
   extends SinglePassRelativeReference with NamedConditional {
 
-  //FIXME: this is not correct for add.forRelativeLabel.
- val instructionSize = 4
+  override def sizeForDistance(distance: Int, offsetDirection: OffsetDirection): Int =
+    encodeForDistance(distance, offsetDirection).size
 
-  override def sizeForDistance(distance: Int, offsetDirection: OffsetDirection): Int = instructionSize
-
-  override def possibleSizes: List[Int] = instructionSize :: Nil
+  override def possibleSizes: Seq[Int] = Seq(4, 8, 12, 16)
 
   override def toString = s"$labelPrefix$mnemonicString $target"
 }
