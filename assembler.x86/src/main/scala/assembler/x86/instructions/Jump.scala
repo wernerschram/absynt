c6cd97211c03a6e8e760a969d97c08199382e0bc
Werner Schram <werner.schram@simacan.com>
2019-01-02 17:53:10 +0100

    added size parameter to FarPointer

--- assembler.x86/src/main/scala/assembler/x86/instructions/Jump.scala
+++ assembler.x86/src/main/scala/assembler/x86/instructions/Jump.scala
@@ -87,16 +87,16 @@ object Jump extends ShortOrLongRelativeJump(0xEB.toByte :: Nil, 0xE9.toByte :: N
   private def RM16(operand: ModRMEncodableOperand)(implicit processorMode: ProcessorMode) =
     new ModRM(operand, 0xff.toByte :: Nil, 4, mnemonic, destination, false) with NoDisplacement with NoImmediate
 
-  private def Ptr1616(farPointer: FarPointer)(implicit processorMode: ProcessorMode) =
-    new Static(0xEA.toByte :: Nil, mnemonic) with FarPointerOperation with NoImmediate {
-      override def pointer: FarPointer = farPointer
+  private def Ptr1616[Size<:FarPointerSize](farPointer: FarPointer with Size)(implicit processorMode: ProcessorMode) =
+    new Static(0xEA.toByte :: Nil, mnemonic) with FarPointerOperation[Size] with NoImmediate {
+      override def pointer: FarPointer with Size = farPointer
     }
 
   private def M1616(operand: MemoryLocation with WideSize)(implicit processorMode: ProcessorMode) =
     new ModRM(operand, 0xFF.toByte :: Nil, 5, s"$mnemonic FAR", destination) with NoDisplacement with NoImmediate
 
   object Far {
-    def apply(farPointer: FarPointer)(implicit processorMode: ProcessorMode): Static with FarPointerOperation =
+    def apply[Size<:FarPointerSize](farPointer: FarPointer with Size)(implicit processorMode: ProcessorMode): Static with FarPointerOperation[Size] =
       Ptr1616(farPointer)
 
     def apply(pointer: MemoryLocation with WideSize)(implicit processorMode: ProcessorMode): ModRM =
--- assembler.x86/src/main/scala/assembler/x86/operations/FarPointer.scala
+++ assembler.x86/src/main/scala/assembler/x86/operations/FarPointer.scala
@@ -1,12 +1,13 @@
 package assembler.x86.operations
 
+import assembler.x86.operands.FarPointerSize
 import assembler.x86.operands.memoryaccess.{FarPointer => FarPointerType}
 import assembler.x86.operations.OperandInfo.OperandOrder._
 
-trait FarPointer extends DisplacementBytes {
+trait FarPointer[Size<:FarPointerSize] extends DisplacementBytes {
 
   self: X86Operation =>
-  def pointer: FarPointerType
+  def pointer: FarPointerType with Size
 
   override protected def displacementInit(): Unit =
     addOperand(OperandInfo.pointer(pointer, destination))
